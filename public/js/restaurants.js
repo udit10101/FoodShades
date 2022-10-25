e2ca3c94ce7f78aa2cac61f80b291343f51343f6
Udit Karan Tomar <uditkarantomar2003@gmail.com>
2022-10-25 21:46:28 +0530

    almostk

--- public/js/restaurants.js
+++ public/js/restaurants.js
@@ -108,7 +108,7 @@ if (document.readyState == 'loading') {
       <span class="cart-price cart-column">${price}</span>
       <div class="cart-quantity cart-column">
           <input class="cart-quantity-input" type="number" value="1">
-          <button class="btn btn-danger" type="button">REMOVE</button>
+          <button class="btn btn-danger" type="button">Remove</button>
       </div>`
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
